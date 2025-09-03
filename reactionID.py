b1e0669f4a5b7ebb713e1c0938f8d6359ea1078f
Ethan Cline <ewcline@mit.edu>
2025-09-03 15:17:06 -0400

    guard against multiple calls

--- reactionID.py
+++ reactionID.py
@@ -12,43 +12,43 @@ from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_prec
 from torch.ao.quantization import fuse_modules
 
 import reactionModel
+if __name__ == "__main__":
+    print("Lets identify some reactions...")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Training on {device}")
 
-print("Lets identify some reactions...")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Training on {device}")
+    def load_txt_data(file_name):
+        decay = []
+        data = []
+        print(f"Loading text data from {file_name}")
+        with open(file_name, 'r') as f:
+            csvReader = csv.reader(f)
+            header = next(csvReader)
+            column = {}
+            for h in header:
+                column[h] = []
+            for row in csvReader:
+                for h, v in zip(header, row):
+                    if h == "MuonDecay":
+                        decay.append(int(v))
+                    else:
+                        column[h].append(v)
 
-def load_txt_data(file_name):
-    decay = []
-    data = []
-    print(f"Loading text data from {file_name}")
-    with open(file_name, 'r') as f:
-        csvReader = csv.reader(f)
-        header = next(csvReader)
-        column = {}
-        for h in header:
-            column[h] = []
-        for row in csvReader:
-           for h, v in zip(header, row):
-               if h == "MuonDecay":
-                   decay.append(int(v))
-               else:
-                   column[h].append(v)
-
-        decay = np.array(decay, dtype=np.float32)
-        data = np.array([column[h] for h in header if h != "MuonDecay"], dtype=np.float32).T
-    return data, decay
+            decay = np.array(decay, dtype=np.float32)
+            data = np.array([column[h] for h in header if h != "MuonDecay"], dtype=np.float32).T
+        return data, decay
 
 
-trainData, trainDecay = load_txt_data("mc17606_mu_positive_training.txt")
 
-validData, validDecay = load_txt_data("mc17606_mu_positive_validation.txt")
+    trainData, trainDecay = load_txt_data("mc17606_mu_positive_training.txt")
+    validData, validDecay = load_txt_data("mc17606_mu_positive_validation.txt")
 
-print(trainData.shape, trainDecay.shape, validData.shape, validDecay.shape)
+    print(trainData.shape, trainDecay.shape, validData.shape, validDecay.shape)
 
-if device.type == "cuda":
-    print(f"Using GPU: {torch.cuda.get_device_name(0)}")
-model = reactionModel.reactionLearner().to(device)
-model, epochs, losses, valid_losses, lr = reactionModel.train_model(model, data=torch.tensor(trainData), truth=torch.tensor(trainDecay), num_epochs=50, device=device, learning_rate=5e-3, savePath="model/output.pth", validData=torch.tensor(validData), validDecay=torch.tensor(validDecay))
+    if device.type == "cuda":
+        print(f"Using GPU: {torch.cuda.get_device_name(0)}")
+    model = reactionModel.reactionLearner().to(device)
+    model, epochs, losses, valid_losses, lr = reactionModel.train_model(model, data=torch.tensor(trainData), truth=torch.tensor(trainDecay), num_epochs=50, device=device, learning_rate=5e-3, savePath="model/output.pth", validData=torch.tensor(validData), validDecay=torch.tensor(validDecay))
 
 
 # if model is None:
